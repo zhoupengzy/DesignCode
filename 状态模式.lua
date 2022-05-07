eb829ddc7ed90da7cc90b1095f06a79d8dddbeb3
zhoupeng <zhoupengzy@163.com>
2022-05-07 14:53:25 +0800

    Fix

--- 状态模式.lua
+++ 状态模式.lua
@@ -1,130 +1,190 @@
 require("functions")
 
-local StateType =
+local ActionStateType =
 {
-    State_Stand = 1,
+    State_Idle = 1,
     State_Run = 2,
     State_Jump = 3,
-    State_Fire = 4,
-    State_Ice = 5,
+    State_Down = 5,
+    State_Weapon_Fire = 6,
+    State_Weapon_Charge = 7,
+    State_Weapon_Switch = 8,
+    State_Weapon_Fire = 9,
 }
 
--- 状态管理
-local StateManger = class("StateManger")
-function StateManger:ctor()
-    self.runState = RunState.new()
-    self.standState = StandState.new()
-    self.jumpState = JumpState.new()
-    self.fireState = FireState.new()
+
+local Hero = class("Hero")
+function Hero:ctor(state1, state2)
+    self.actionState = state1
+    self.weaponState = state2
+end
+-- action
+function Hero:Run()
+    print("英雄正在跑动")
+end
+function Hero:Jump()
+    print("英雄正在跳跃")
+end
+function Hero:Idle()
+    print("英雄正在站立")
+end
+function Hero:Down()
+    print("英雄正在卧倒")
+end
+-- 
+function Hero:Fire()
+    print("英雄正在开火")
+end
+
+function Hero:ChangeState(state)
+    local state = self.actionState:ChangeState(state)
+    if state then
+        self.actionState = state
+        self.actionState:Enter(self)
+    end
+    -- state = self.weaponState:ChangeState(state)
+    -- if state then
+    --     self.weaponState = state
+    --     self.weaponState:Enter(self)
+    -- end
+end
+function Hero:Update()
+    self.actionState:Update(self )
 end
 
 
 -- 接口
 local ActionState = class("ActionState")
-function ActionState:ChangeState(hero, state)
+function ActionState:ctor()
+end
+function ActionState:ChangeState(state)
+end
+function ActionState:Update(hero)
 end
 
-local StandState = class("StandState", ActionState)
-function StandState:ctor(hero)
-    hero.state = StateType.State_Stand
+
+local IdleState = class("IdleState", ActionState)
+function IdleState:ctor()
+    self.stateType = ActionStateType.State_Idle
 end
-function StandState:Enter(hero)
-    hero:Stand()
+function IdleState:Enter(hero)
+    hero:Idle()
 end
-function StandState:ChangeState(hero, state)
-    if state == StateType.State_Fire then
-        hero.actionState = FireState.new(hero)
-    elseif state == StateType.State_Jump then
-        hero:Jump()
-    elseif state == StateType.State_Run then
-        hero:Run()
+function IdleState:ChangeState(state)
+    if state.stateType == ActionStateType.State_Fire 
+            or state.stateType == ActionStateType.State_Jump 
+            or state.stateType == ActionStateType.State_Run then
+        return state
+    else 
+        return nil
     end
 end
 
 local RunState = class("RunState", ActionState)
+function RunState:ctor()
+    self.stateType = ActionStateType.State_Run
+end
 function RunState:Enter(hero)
     hero:Run()
 end
-function RunState:ChangeState(hero, state)
-    if state == StateType.State_Fire then
-        hero:Fire()
-    elseif state == StateType.State_Jump then
-        hero:Jump()
-    elseif state == StateType.State_Stand then
-        hero:Stand()
+function RunState:ChangeState(state)
+    if state.stateType == ActionStateType.State_Fire
+            or state.stateType == ActionStateType.State_Jump
+            or state.stateType == ActionStateType.State_Idle then
+        return state
+    else
+        return nil
     end
 end
 
 local JumpState = class("JumpState", ActionState)
+function JumpState:ctor()
+    self.stateType = ActionStateType.State_Jump
+    self.runTime = 0
+end
 function JumpState:Enter(hero)
     hero:Jump()
 end
-function JumpState:ChangeState(hero, state)
-    if state == StateType.State_Fire then
-        hero:Fire()
+function JumpState:ChangeState(state)
+    if state.stateType == ActionStateType.State_Fire then
+        return state
+    elseif state.stateType == ActionStateType.State_Idle
+        and self.runTime >= 1 then
+        return state
+    else
+        return nil
     end
 end
 function JumpState:Update(hero)
-    self.changeTime = self.changeTime + 1
-    if self.changeTime >= 1 then
-        hero:Stand()
+    self.runTime = self.runTime + 0.1
+    if self.runTime >= 1 then
+        hero:ChangeState(IdleState.new())
     end
 end
 
-local FireState = class("FireState", ActionState)
-function FireState:Enter(hero)
-    hero:Fire()
+local StateDown = class("StateDown", ActionState)
+function StateDown:ctor()
+    self.stateType = ActionStateType.State_Down
 end
-function FireState:ChangeState(hero, state)
-    if state == StateType.State_Stand then
-        hero:Stand()
+function StateDown:Enter(hero)
+    hero:Down()
+end
+function StateDown:ChangeState(state)
+   if state.stateType == ActionStateType.State_Idle then
+        return state
+    else
+        return nil
     end
 end
 
-local Hero = require("Hero")
-function Hero:ctor()
-    self.actionState = nil
+local StateManager =  class("StateManager")
+function StateManager:ctor()
+    self.manager = {}
 end
-function Hero:Run()
+function StateManager:GetState(stateType)
+    if self.manager[stateType] then
+        return self.manager[stateType]
+    else
+        if stateType == ActionStateType.State_Run then
+            return RunState.new()
+        elseif stateType == ActionStateType.State_Jump then
+            return JumpState.new()
+        elseif stateType == ActionStateType.State_Idle then
+            return IdleState.new()
+        else
+            return nil
+        end
+    end
 end
-function Hero:Jump()
-end
-function Hero:Fire()
-end
-function Hero:Stand()
-end
--- function Hero:Update(state)
---     if self.actionState.state == StateType.State_Fire then
---         self:Fire()
---     elseif state == StateType.State_Jump then
---         self:Jump()
---     elseif state == StateType.State_Run then
---         self:Run()
---     elseif state == StateType.State_Stand then
---         self:Stand()
---     end
--- end
 
-
-
-
-local stateManager = StateManger.new()
-local hero = Hero.new()
+local stateManager = StateManager.new()
+local hero = Hero.new(stateManager:GetState(ActionStateType.State_Idle))
 
 function KeyInput(input)
-    if input == PRESS_A or intpu == PRESS_D then
-        hero.actionState:ChangeState(self.hero, StateType.State_Run)
-    elseif input == PRESS_W then
-        hero.actionState:ChangeState(self.hero, StateType.State_Jump)
-    elseif input == PRESS_S then
-        hero.actionState:ChangeState(self.hero, StateType.State_Stand)
-    elseif input == PRESS_SPACE then
-        hero.actionState:ChangeState(self.hero, StateType.State_Fire)
+    if input == "PRESS_A" or intpu == "PRESS_D" then
+        hero:ChangeState(stateManager:GetState(ActionStateType.State_Run))
+    elseif input == "PRESS_W" then
+        hero:ChangeState(stateManager:GetState(ActionStateType.State_Jump))
+    elseif input == "PRESS_S" then
+        hero:ChangeState(stateManager:GetState(ActionStateType.State_Idle))
     end
 end
 
+print("开始游戏")
+KeyInput("PRESS_A")
+KeyInput("PRESS_W")
+KeyInput("PRESS_S")
+KeyInput("PRESS_A")
+for i=1,20 do
+    KeyInput("PRESS_A")
+    hero:Update()
+end
+KeyInput("PRESS_S")
+KeyInput("PRESS_A")
+
+print("结束游戏")
 
 -- 总结：对象的行为是基于它的状态改变的
 -- 优点：
 -- 缺点：
--- 场景：
+-- 场景：玩家控制,怪物AI,UI状态,流程控制
